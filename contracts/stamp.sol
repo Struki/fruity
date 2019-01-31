6230cf489f8a96e0385cc848fb3cddb6c7f7d192
Struki <kurtis794@gmail.com>
2019-01-31 11:41:37 +0100

    Zeppelin rules!

--- contracts/stamp.sol
+++ contracts/stamp.sol
@@ -1,25 +1,27 @@
-pragma solidity ^0.4.24;
 
-contract Item is VIP181{
-    
-    struct Item{
-        string name; // Name of the Item
-        uint level; // Item Level
-        uint rarityLevel;  // 1 = normal, 2 = rare, 3 = epic, 4 = legendary
-    }
-    
-    Item[] public items; // First Item has Index 0
-    address public owner;
-    
-    function Item() public {
-        owner = msg.sender; // The Sender is the Owner; Ethereum Address of the Owner
-    }
-    
-    function createItem(string _name, address _to) public{
-        require(owner == msg.sender); // Only the Owner can create Items
-        uint id = items.length; // Item ID = Length of the Array Items
-        items.push(Item(_name,5,1)) // Item ("Sword",5,1)
-        _mint(_to,id); // Assigns the Token to the Ethereum Address that is specified
-    }
-    
+pragma solidity ^0.4.21;
+
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+
+contract RoboHashToken is VIP181Token("Ingredients", "ING") {
+	
+	mapping(uint256 => string) internal tokenIdToName;
+	mapping(string => uint256) internal nameToTokenId;
+
+	function create(string name) public {
+		require(nameToTokenId[name] == 0);
+		uint256 tokenId = allTokens.length + 1;
+		_mint(msg.sender, tokenId);
+		tokenIdToName[tokenId] = name;
+		nameToTokenId[name] = tokenId;
+	}
+
+	function getTokenName(uint256 tokenId) view public returns (string) {
+		return tokenIdToName[tokenId];
+	}
+
+	function getTokenId(string name) view public returns (uint) {
+		return nameToTokenId[name];
+	}
 }
+
