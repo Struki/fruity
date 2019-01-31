f75b62799350d205a9c2778adef4fe1bd4b76772
Struki <kurtis794@gmail.com>
2019-01-31 13:56:34 +0100

    Zeppelin Rules, Stairway to Fungibility

--- contracts/stamp.sol
+++ contracts/stamp.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.21;
 
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 
-contract RoboHashToken is VIP181Token("Ingredients", "ING") {
+contract Stamp is VIP181Token("Ingredients", "ING") {
 	
 	mapping(uint256 => string) internal tokenIdToName;
 	mapping(string => uint256) internal nameToTokenId;
---
+++ migrations/3_deploy_contracts.js
@@ -0,0 +1,7 @@
+var Stamps = artifacts.require("Stamps");
+
+
+module.exports = function(deployer) {
+
+  deployer.deploy(Stamps);
+};
